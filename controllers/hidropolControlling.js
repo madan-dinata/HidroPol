42cd396de8d3622862f11fcc7e775e51b43fbb72
madan-dinata <madanmr48@gmail.com>
2022-01-05 17:22:58 +0700

    initial commit

--- controllers/hidropolControlling.js
+++ controllers/hidropolControlling.js
@@ -1,21 +1,3 @@
-// require('dotenv').config()
-// const firebase = require('firebase/firebase-app')
-// require('firebase/auth');
-// require('firebase/database');
-
-// const firebaseConfig = {
-//     apiKey: process.env.APIKEY,
-//     authDomain: process.env.AUTHDOMAIN,
-//     databaseURL: process.env.DATABASEURL,
-//     projectId: process.env.PROJECTID,
-//     storageBucket: process.env.STORAGEBUCKET,
-//     messagingSenderId: process.env.MESSAGINGSENDERID,
-//     appId: process.env.APPID
-// };
-// const app = firebase(firebaseConfig)
-
-// module.exports = app
-
 exports.index = (req, res) => {
         res.render("index", { title: "Hidropol" });
 }
